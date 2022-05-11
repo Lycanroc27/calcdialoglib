1c30c5abd08f60328b3358c9b3684cfad1f5db06
Saurav Dev <badhan1807027@stud.kuet.ac.bd>
2022-05-11 11:27:08 +0600

    done

---
+++ newclass.java
@@ -0,0 +1,19 @@
+public newclass{
+        int a,b;
+public:
+        newclass(int a,b){
+        this.a=a;
+        this.b=b;
+        }
+        void geta(){
+        return a;
+        }
+        void getb(){
+        return b;
+        }
+        void seta{
+        this.a=a;
+        }void setb{
+        this.b=b;
+        }
+        };
